15554595ace9f6976bd9c928630a3a247f99eea5
angryatti <angry@gmail.com>
2023-11-11 08:45:16 +0100

    initial decimal ipv4 to binary ip

--- binaryultimate.cpp
+++ binaryultimate.cpp
@@ -1,5 +1,7 @@
 #include "binaryultimate.h"
 #include "./ui_binaryultimate.h"
+#include <QMessageBox>
+#include <QRegularExpression>
 
 BinaryUltimate::BinaryUltimate(QWidget *parent)
     : QMainWindow(parent)
@@ -30,7 +32,7 @@ int BinToDec(int binNumber) {
 
 QString DecToBin(QString decNumber)
     {
-    int a[10], n, i;
+    int a[10], n, i =0;
     QString fDecN="";
     n = decNumber.toInt();
 for(i=0; n>0; i++)
@@ -48,9 +50,68 @@ return fDecN;
  }
 
 
-void BinaryUltimate::on_ToBinaryBT1_clicked()
+ void BinaryUltimate::on_ToBinaryBT1_clicked()
+ {
+QStringList octList;
+octList << ui->ipLE1->text().split('.');
+QMessageBox msgBox;
+QRegularExpression regEx("[0-9]");
+QRegularExpressionMatch match;
+
+for (int i =0;i<octList.length();i++)
 {
+  match =  regEx.match(octList[i]);
 
-ui->outBinL2->setText(DecToBin(ui->ipLE1->text()));
+    if (!match.hasMatch())
+    {
+      msgBox.setText("Invalid ip or subnetmask it is containing alphabet");
+      msgBox.exec();
+      return;
+
+
+    }
+    if (octList[i].toInt()>255)
+    {
+    msgBox.setText("Invalid ip or subnetmask");
+    msgBox.exec();
+    return;
+
+
+    }
 }
+int cnt = 0;
+QString tempIP = ui->ipLE1->text();
+for (int i=0;i<tempIP.length();i++)
+         {
+    if (tempIP[i]=='.')
+  {
+    cnt++;
 
+    }
+    if (cnt>3)
+    {
+    break;
+    }
+
+    }
+if (cnt>3)
+{
+    msgBox.setText("Invalid ip or subnetmask");
+    msgBox.exec();
+    return;
+
+}
+QString outBinIp ="";
+for (int i =0;i<=3;i++)
+    {
+    qDebug() << octList[i];
+     outBinIp+= QStringLiteral("%1").arg(DecToBin(octList[i]),8,QLatin1Char('0')) +".";
+
+
+}
+outBinIp.removeAt(outBinIp.length()-1);
+qDebug() << outBinIp;
+
+
+ui->binaryIPLE2->setText(outBinIp);
+ }
